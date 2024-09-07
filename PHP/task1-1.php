af5fa33412a2f51ee0e866be220b69c9b962bfb0
Sae-72 <otomonattsu@gmail.com>
2024-09-08 08:08:11 +0900

    task1-1完成

---
+++ PHP/task1-1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP練習</title>
+</head>
+<body>
+  <?php
+  $a = "暑中お見舞い";
+  $b = "申し上げます。";
+  ?>
+
+  <p><?= $a.$b ?></p> 
+  <?= $a ?><br>
+  <?= $b ?>
+</body>
+</html>
