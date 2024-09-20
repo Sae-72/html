6f47ac999bb9248170d0f57f2a416b95ed0d70a5
Sae-72 <otomonattsu@gmail.com>
2024-09-20 12:45:36 +0900

    task8-1途中

--- contact/task8-1.php
+++ contact/task8-1.php
@@ -1,3 +1,41 @@
+<?php
+if (empty($_POST['name'])){
+  $error[] = "名前を入力してください。";
+} else {
+  $name = $_POST['name'];
+}
+if (empty($_POST['furigana'])){
+  $error[] = "フリガナを入力してください。";
+} else {
+  $furigana = $_POST['furigana'];
+}
+if (empty($_POST['email'])){
+  $error[] = "アドレスを入力してください。";
+} else {
+  $email = $_POST['email'];
+}
+if (empty($_POST['phone'])){
+  $error[] = "電話番号を入力してください。";
+} else {
+  $phone = $_POST['phone'];
+}
+if (empty($_POST['select'])){
+  $error[] = "選択してください。";
+} else {
+  $select = $_POST['select'];
+}
+if (empty($_POST['message'])){
+  $error[] = "内容をを入力してください。";
+} else {
+  $message = $_POST['message'];
+}
+if (empty($_POST['check'])){
+  $error[] = "個人情報保護方針に同意してください。";
+} else {
+  $check = $_POST['check'];
+}
+
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -29,29 +67,36 @@
     <section class="contact-form">
       <div class="contact-form_top">
         <h2>お問い合わせ</h2>
-        <p>お問い合わせや業務内容に関するご質問は、電話またはこちらのお問い合わせフォームより承っております。</p><span><p>後ほど担当者よりご連絡させていただきます。</p></span>
+        <p>お問い合わせや業務内容に関するご質問は、電話またはこちらのお問い合わせフォームより承っております。</p><span><p>後ほど担当者よりご連絡させていただきます。</p><br>
+        <p><?php if (!empty($error)): ?>
+        <ul class="error-messages" style="color: red;text-align: center;">
+            <?php foreach ($error as $err): ?>
+                <li><?= $err ?></li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?></p></span>
         </div>
         <form action="task8-1.php" method="POST">
           <div class="form-group">
               <label for="name">お名前 <span class="required">必須</span></label>
-              <input type="text" id="name" name="name" placeholder="山田太郎">
+              <input type="text" id="name" name="name" placeholder="山田太郎" value = "<?= $name ?>">
           </div>
           <div class="form-group">
               <label for="furigana">フリガナ <span class="required">必須</span></label>
-              <input type="text" id="furigana" name="furigana" placeholder="ヤマダタロウ">
+              <input type="text" id="furigana" name="furigana" placeholder="ヤマダタロウ" value = "<?= $furigana ?>">
           </div>
           <div class="form-group">
               <label for="email">メールアドレス <span class="required">必須</span></label>
-              <input type="email" id="email" name="email" placeholder="info@fast-creademy.jp">
+              <input type="email" id="email" name="email" placeholder="info@fast-creademy.jp" value = "<?= $email ?>">
           </div>
           <div class="form-group">
               <label for="phone">電話番号 <span class="required">必須</span></label>
-              <input type="tel" id="phone" name="phone" placeholder="090-1234-5678">
+              <input type="tel" id="phone" name="phone" placeholder="090-1234-5678" value = "<?= $phone ?>">
           </div>
           <div class="form-group">
               <label for="inquiry">お問い合わせ項目 <span class="required">必須</span></label>
               <select id="inquiry" name="inquiry">
-                  <option value="">選択してください</option>
+                  <option value="" <?= empty($select) ?>>選択してください</option>
                   <option value="質問">質問</option>
                   <option value="要望">要望</option>
                   <option value="その他">その他</option>
@@ -59,12 +104,12 @@
           </div>
           <div class="form-group">
               <label for="message">お問い合わせ内容<span class="required">必須</span></label>
-              <textarea id="message" name="message" placeholder="こちらにお問い合わせ内容をご記入ください" rows="5"></textarea>
+              <textarea id="message" name="message" placeholder="こちらにお問い合わせ内容をご記入ください" rows="5"><?= $message ?></textarea>
           </div>
           <div class="form-group_01">
               <label for="privacy">個人情報保護方針 <span class="required">必須</span></label>
               <div class="check_privacy">
-                <input type="checkbox" id="privacy" name="privacy"><a href="#">個人情報保護方針</a><i class="fa-solid fa-window-restore"></i>に同意します。
+                <input type="checkbox" id="privacy" name="privacy" value="1" <?= empty($check) ?>><a href="#">個人情報保護方針</a><i class="fa-solid fa-window-restore"></i>に同意します。
               </div>
           </div>
           <button type="submit">確認</button>
