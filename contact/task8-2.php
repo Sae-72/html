2dffe20f6337d37bf4f2ba997577c10de8689da2
Sae-72 <otomonattsu@gmail.com>
2024-09-26 22:25:34 +0900

    送信完了ページを改善。

--- contact/task8-2.php
+++ contact/task8-2.php
@@ -3,18 +3,66 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="reset.css">
+  <link rel="stylesheet" href="style.css">
   <title>送信完了</title>
 </head>
 <body>
-  <h1>送信完了しました。</h1>
-  <style>
-    body, html {
-      height: 100%;
-      margin: 0;
-      display: flex;
-      justify-content: center; 
-      align-items: center; 
-    }
-  </style>
+<header>
+    <h1>ここには会社名が入ります</h1>
+    <ul class="header_btn">
+      <li>
+        <a href="#" class="btn_01">ボタン01</a>
+      </li>
+      <li>
+        <a href="#" class="btn_02">ボタン02</a>
+      </li>
+    </ul>
+  </header>
+  <div class="menu">
+    <a href="#" class="menu_01">メニュー01</a>
+    <a href="#" class="menu_02">メニュー02</a>
+    <a href="#" class="menu_03">メニュー03</a>
+  </div>
+    <img src="img/mv.png" alt="">
+    <section class="contact-form">
+      <div class="contact-form_top">
+        <h2>お問い合わせ</h2>
+        <h2 style="color: blue;text-align: center;padding: 15px 0;">送信完了しました。</h2>
+        </div>      
+        <section class="sec_05">
+          <div class="sec_05_content">
+            <div class="sec_05_left">
+              <h2>こちらからご購入ください</h2>
+              <a href="#" class="btn_n">ネットショップ</a>
+            </div>
+          <div class="sec_05_right">
+            <h2>お気軽にお問い合わせください</h2>
+            <a href="#" class="btn_o">お問い合せ</a>
+          </div>
+          </div>
+        </section>
+        <footer>
+          <div class="foot_01">
+            <h4>ここには会社名が入ります</h4>
+            <p>住所が入ります</p>
+            <p>03-1234-5678</p>
+            <p>営業時間：9：00～18：00</p>
+          </div>
+          <ul class="foot_02">
+            <li> <a href="#">リンク01</a></li>
+            <li> <a href="#">リンク02</a></li>
+            <li> <a href="#">リンク03</a></li>
+            <li> <a href="#">リンク04</a></li>
+            <li> <a href="#">リンク05</a></li>
+            <div>
+              <li> <a href="#">リンク06</a></li>
+              <li> <a href="#">リンク07</a></li>        
+            </div>
+          </ul>
+          <div class="foot_03">
+            <p>ここには会社名が入ります©Copyright</p>
+          </div>
+        </footer>
 </body>
 </html>
